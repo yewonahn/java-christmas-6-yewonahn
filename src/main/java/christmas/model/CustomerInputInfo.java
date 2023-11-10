1268ee70722ec590efa8840b9c74442b0b44f32c
yewonahn <yewonahn@naver.com>
2023-11-11 04:13:03 +0900

    feat: 각 이벤트 적용 조건에 부합하는지 확인하는 check- 메서드 추가

--- src/main/java/christmas/model/CustomerInputInfo.java
+++ src/main/java/christmas/model/CustomerInputInfo.java
@@ -1,13 +1,44 @@
 package christmas.model;
 
-public class CustomerInputInfo {
-    private final String orderMenu;
-    private final int orderQuantity;
-    private final int visitDate;
+import christmas.enums.Menu;
+import christmas.service.GetDayOfTheWeek;
 
-    public CustomerInputInfo(String orderMenu, int orderQuantity, int visitDate) {
-        this.orderMenu = orderMenu;
-        this.orderQuantity = orderQuantity;
+import java.util.List;
+
+public class CustomerInputInfo {
+    private final List<Order> orders;
+    private final int visitDate;
+    private final String dayOfTheWeek;
+
+    public CustomerInputInfo(List<Order> orders, int visitDate) {
+        this.orders = orders;
         this.visitDate = visitDate;
+        this.dayOfTheWeek = GetDayOfTheWeek.getDayOfTheWeek(visitDate);
+    }
+    public boolean checkChristmas() {
+        if (visitDate == 25) {
+            return true;
+        }
+        return false;
+    }
+    public boolean checkSpecialDay() {
+        if (visitDate % 7 == 3 || visitDate == 25) {
+            return true;
+        }
+        return false;
+    }
+    public boolean checkPresent() {
+        int totalPriceBeforeDiscount = getTotalPriceBeforeDiscount();
+        if (totalPriceBeforeDiscount >= 120000) {
+            return true;
+        }
+        return false;
+    }
+    public int getTotalPriceBeforeDiscount() {
+        int totalPriceBeforeDiscount = 0;
+        for(Order order : orders) {
+            totalPriceBeforeDiscount += order.getEachMenuTotalPrice();
+        }
+        return totalPriceBeforeDiscount;
     }
 }
