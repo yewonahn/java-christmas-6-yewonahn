c5b956032ea130645269d662d868e59d0b57c7ca
yewonahn <yewonahn@naver.com>
2023-11-11 02:18:55 +0900

    style: CustomerInputInfo 이름 변경

--- src/main/java/christmas/model/CustomerInputInfo.java
+++ src/main/java/christmas/model/CustomerInputInfo.java
@@ -1,11 +1,11 @@
 package christmas.model;
 
-public class CustomerInputInform {
+public class CustomerInputInfo {
     private final String orderMenu;
     private final int orderQuantity;
     private final int visitDate;
 
-    public CustomerInputInform(String orderMenu, int orderQuantity, int visitDate) {
+    public CustomerInputInfo(String orderMenu, int orderQuantity, int visitDate) {
         this.orderMenu = orderMenu;
         this.orderQuantity = orderQuantity;
         this.visitDate = visitDate;
