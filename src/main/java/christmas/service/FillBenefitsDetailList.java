2dd1d48447761c4e76fa615aaa498dd55bfcbaa3
yewonahn <yewonahn@naver.com>
2023-11-12 00:28:48 +0900

    feat: [고객 주문 사항 리스트] 채우는 클래스 생성

---
+++ src/main/java/christmas/service/FillBenefitsDetailList.java
@@ -0,0 +1,55 @@
+package christmas.service;
+
+import christmas.model.EachBenefitDetail;
+
+import java.util.List;
+
+import static christmas.Application.customerInputInfo;
+
+public class FillBenefitsDetailList {
+    public static void christmas(List<EachBenefitDetail> benefitDetails) {
+        if(customerInputInfo.checkChristmas()) {
+            int benefitPrice = CalculateBenefitAmount.dDay(customerInputInfo);
+            EachBenefitDetail eachBenefitDetail = new EachBenefitDetail("크리스마스 디데이 할인", benefitPrice);
+            benefitDetails.add(eachBenefitDetail);
+            return;
+        }
+        return;
+    }
+    public static void weekday(List<EachBenefitDetail> benefitDetails) {
+        if(customerInputInfo.checkWeekday()) {
+            int benefitPrice = CalculateBenefitAmount.weekday(customerInputInfo);
+            EachBenefitDetail eachBenefitDetail = new EachBenefitDetail("평일 할인", benefitPrice);
+            benefitDetails.add(eachBenefitDetail);
+            return;
+        }
+        return;
+    }
+    public static void weekend(List<EachBenefitDetail> benefitDetails) {
+        if(customerInputInfo.checkWeekend()) {
+            int benefitPrice = CalculateBenefitAmount.weekend(customerInputInfo);
+            EachBenefitDetail eachBenefitDetail = new EachBenefitDetail("주말 할인", benefitPrice);
+            benefitDetails.add(eachBenefitDetail);
+            return;
+        }
+        return;
+    }
+    public static void specialDay(List<EachBenefitDetail> benefitDetails) {
+        if(customerInputInfo.checkSpecialDay()) {
+            int benefitPrice = CalculateBenefitAmount.specialDay();
+            EachBenefitDetail eachBenefitDetail = new EachBenefitDetail("특별 할인", benefitPrice);
+            benefitDetails.add(eachBenefitDetail);
+            return;
+        }
+        return;
+    }
+    public static void present(List<EachBenefitDetail> benefitDetails) {
+        if(customerInputInfo.checkPresent()) {
+            int benefitPrice = CalculateBenefitAmount.present();
+            EachBenefitDetail eachBenefitDetail = new EachBenefitDetail("증정 이벤트", benefitPrice);
+            benefitDetails.add(eachBenefitDetail);
+            return;
+        }
+        return;
+    }
+}
