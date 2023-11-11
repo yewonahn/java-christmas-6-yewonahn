55c0cbbbe378f0dd8f70e04703cc293ac8c421c0
yewonahn <yewonahn@naver.com>
2023-11-12 04:54:30 +0900

    fix: 총혜택 금액, 총할인 금액 구하는 메서드에서 평일, 주말 구분

--- src/main/java/christmas/service/CalculateBenefitAmount.java
+++ src/main/java/christmas/service/CalculateBenefitAmount.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static christmas.Application.customerInputInfo;
 
 public class CalculateBenefitAmount {
-    public static int dDay (CustomerInputInfo customerInputInfo) {
+    public static int christmasDDay (CustomerInputInfo customerInputInfo) {
         int visitDate = customerInputInfo.getVisitDate();
         int dDayBenefit = 1000 + (visitDate - 1) * 100;
         return dDayBenefit;
@@ -18,7 +18,7 @@ public class CalculateBenefitAmount {
         int weekdayBenefit = 0;
         for (Order order : orders) {
             if (order.getCategory() == "<디저트>") {
-                weekdayBenefit += 2023;
+                weekdayBenefit += 2023 * order.getOrderQuantity();
             }
         }
         return weekdayBenefit;
@@ -42,7 +42,23 @@ public class CalculateBenefitAmount {
         return presentBenefit;
     }
     public static int totalBenefit() {
-             int totalBenefit = CalculateBenefitAmount.dDay(customerInputInfo) + CalculateBenefitAmount.weekday(customerInputInfo) + CalculateBenefitAmount.weekend(customerInputInfo) + CalculateBenefitAmount.specialDay() + CalculateBenefitAmount.present();
-             return totalBenefit;
+        int totalBenefit = 0;
+        if (customerInputInfo.checkWeekday()) {
+            totalBenefit = CalculateBenefitAmount.christmasDDay(customerInputInfo) + CalculateBenefitAmount.weekday(customerInputInfo) + CalculateBenefitAmount.specialDay() + CalculateBenefitAmount.present();
+        }
+        if (customerInputInfo.checkWeekend()) {
+            totalBenefit = CalculateBenefitAmount.christmasDDay(customerInputInfo) + CalculateBenefitAmount.weekend(customerInputInfo) + CalculateBenefitAmount.specialDay() + CalculateBenefitAmount.present();
+        }
+        return totalBenefit;
+    }
+    public static int totalDiscount() {
+        int totalDiscount = 0;
+        if (customerInputInfo.checkWeekday()) {
+            totalDiscount = CalculateBenefitAmount.christmasDDay(customerInputInfo) + CalculateBenefitAmount.weekday(customerInputInfo) + CalculateBenefitAmount.specialDay();
+        }
+        if (customerInputInfo.checkWeekend()) {
+            totalDiscount = CalculateBenefitAmount.christmasDDay(customerInputInfo) + CalculateBenefitAmount.weekend(customerInputInfo) + CalculateBenefitAmount.specialDay();
+        }
+        return totalDiscount;
     }
 }
