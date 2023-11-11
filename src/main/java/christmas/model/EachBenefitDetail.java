028d0e451bb1aa833be5987ca51a7d1cf37007e8
yewonahn <yewonahn@naver.com>
2023-11-12 01:05:48 +0900

    feat: 혜택 내역(이벤트 이름, 고객이 받게 되는 혜택 금액)을 담는 객체 클래스 생성

---
+++ src/main/java/christmas/model/EachBenefitDetail.java
@@ -0,0 +1,11 @@
+package christmas.model;
+
+public class EachBenefitDetail {
+    private String eventName;
+    private int benefitPrice;
+
+    public EachBenefitDetail(String eventName, int benefitPrice) {
+        this.eventName = eventName;
+        this.benefitPrice = benefitPrice;
+    }
+}
