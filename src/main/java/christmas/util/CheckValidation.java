9ee4e4c70935db76aaaaf5de93f0e19b2d887ad9
yewonahn <yewonahn@naver.com>
2023-11-13 04:36:16 +0900

    fix: 1. InputView 변경 사항에 맞춰 수정 2 .사용자 입력 오류로 인한 NumberFormatException 발생 시 IllegalArgumentException으로 처리

--- src/main/java/christmas/util/CheckValidation.java
+++ src/main/java/christmas/util/CheckValidation.java
@@ -5,50 +5,44 @@ import christmas.model.Order;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static christmas.view.InputView.inputOrderMenu;
 
 public class CheckValidation {
-    public void checkDateType(String stringVisitDate) {
-        if (Integer.parseInt(stringVisitDate) < 1 || Integer.parseInt(stringVisitDate) > 31) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-        }
-    }
-    public List<Order> checkInputOrderMenu(List<Order> orders) {
+    public static void checkDateType(String stringVisitDate) {
         try {
-            checkMenuValid(orders);
-            checkMenuAmount(orders);
-            checkMenuDuplication(orders);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return inputOrderMenu();
+            int date = Integer.parseInt(stringVisitDate);
+            if (date < 1 || date > 31) {
+                throw new IllegalArgumentException();
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
         }
-        return orders;
     }
-    public void checkMenuValid(List<Order> orders) {
-        for (Order order : orders) {
+    public static void checkMenuAmount(String stringQuantity) {
+        try {
+            int quantity = Integer.parseInt(stringQuantity);
+            if (quantity < 1 || quantity > 20) {
+                throw new IllegalArgumentException();
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public static void checkMenuValid(List<Order> orders) {
+        for(Order order : orders) {
             if(! Menu.checkRightName(order.getOrderMenu())) {
-                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+                throw new IllegalArgumentException();
             }
         }
     }
-    public void checkMenuAmount(List<Order> orders) {
-        for (Order order : orders) {
-            if (order.getOrderQuantity() < 1 || order.getOrderQuantity() > 20) {
-                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-            }
-        }
-    }
-    public void checkMenuDuplication(List<Order> orders) {
+
+    public static void checkMenuDuplication(List<Order> orders) {
         List<String> menuNames = new ArrayList<>();
         for(Order order : orders) {
             menuNames.add(order.getOrderMenu());
         }
-        List<String> duplicationTest = new ArrayList<>();
-        duplicationTest = menuNames.stream().distinct().collect(Collectors.toList());
+        List<String> duplicationTest = menuNames.stream().distinct().toList();
         if (menuNames.size() != duplicationTest.size()) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException();
         }
     }
 }
